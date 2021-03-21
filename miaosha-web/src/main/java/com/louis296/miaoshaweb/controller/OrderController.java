dc410833013100194bdc0a68ac4303f4d3155eb8
Louis296 <1zhaozhiwei@gmail.com>
2021-03-21 23:04:50 +0800

    基于RateLimiter实现了一个令牌桶

--- miaosha-web/src/main/java/com/louis296/miaoshaweb/controller/OrderController.java
+++ miaosha-web/src/main/java/com/louis296/miaoshaweb/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.louis296.miaoshaweb.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.louis296.miaoshaservice.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.TimeUnit;
+
 @Controller
 public class OrderController {
 
@@ -16,6 +19,8 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    RateLimiter rateLimiter=RateLimiter.create(10);
 
     @RequestMapping("/createWrongOrder/{sid}")
     @ResponseBody
@@ -31,9 +36,19 @@ public class OrderController {
         return String.valueOf(id);
     }
 
+    /*
+        使用令牌桶＋乐观锁
+     */
     @RequestMapping("/createOptimisticOrder/{sid}")
     @ResponseBody
     public String createOptimisticOrder(@PathVariable int sid) {
+//         阻塞式获取令牌
+         LOGGER.info("等待时间"+rateLimiter.acquire());
+//         非阻塞式获取令牌
+//        if(!rateLimiter.tryAcquire(1000, TimeUnit.MILLISECONDS)){
+//            LOGGER.warn("对不起，您已被限流");
+//            return "购买失败，库存不足";
+//        }
         int id;
         try {
             id = orderService.createOptimisticOrder(sid);
